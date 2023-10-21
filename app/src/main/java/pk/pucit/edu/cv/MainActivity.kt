32b34627d32df1a0a35f7556126957b8704be991
NUMEER QADRI <qadrinumeer@gmail.com>
2023-10-21 09:32:47 +0500

    Initial Commit

---
+++ app/src/main/java/pk/pucit/edu/cv/MainActivity.kt
@@ -0,0 +1,11 @@
+package pk.pucit.edu.cv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
